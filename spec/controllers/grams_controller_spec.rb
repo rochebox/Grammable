fdb8c5c8733d49b47e076937067e88ef748f3019
rochebox <rochebox@gmail.com>
2016-09-06 07:43:28 +0000

    Lesson 10 Connect the gram to the user

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -5,20 +5,44 @@ RSpec.describe GramsController, type: :controller do
 		it "should successfully show the page" do
 			get :index
 			expect(response).to have_http_status(:success)
-
 		end
 	end
 
+
 	describe "grams#new action" do
-		it "should successfully show the form" do
+		it "should require users to be logged in" do
+			get :new
+			expect(response).to redirect_to new_user_session_path
+		end
+
+		it "should successfully show the new form" do
+     	  	user = User.create(
+        		email:                 'fakeuser@gmail.com',
+        		password:              'secretPassword',
+        		password_confirmation: 'secretPassword'
+      	 	)
+      	 	sign_in user
+
 			get :new
 			expect(response).to have_http_status(:success)
 		end
 	end
 
 	describe "grams#create action" do
+		it "should require users to be logged in" do
+    		post :create, gram: { message: "Hello" }
+    		expect(response).to redirect_to new_user_session_path
+  		end
+
 		it "should sucessfully create a new gram in our database" do
-			post :create, gram: {message: 'Hello!'}
+			user = User.create(
+        		email:                 'fakeuser@gmail.com',
+        		password:              'secretPassword',
+        		password_confirmation: 'secretPassword'
+      		)
+      		sign_in user
+
+			post :create, gram: { message: 'Hello!' }
 			expect(response).to redirect_to root_path
 
 			gram = Gram.last
@@ -26,12 +50,19 @@ RSpec.describe GramsController, type: :controller do
 		end
 
 		it "should properly deal with validation errors" do
+			user = User.create(
+        		email:                 'fakeuser@gmail.com',
+        		password:              'secretPassword',
+        		password_confirmation: 'secretPassword'
+      		)
+      		sign_in user
+
+      		gram_count = Gram.count
 			post :create, gram: { message: '' }
 			expect(response).to have_http_status(:unprocessable_entity)
 			
-			expect(Gram.count).to eq 0
+			expect(Gram.count).to eq Gram.count
 		end
 
 	end
-
 end

1df15b73f12886fd3f3c04fe433c165f8e2aea1e
rochebox <rochebox@gmail.com>
2016-09-06 04:32:36 +0000

    Lesson 9 -- Gram Validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Grammable::Application.routes.draw do
+  devise_for :users
   root "grams#index"
   resources :grams, only: [:new, :create]
   #resources :grams, only: :create  didnt do this wanted more general
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -20,9 +20,18 @@ RSpec.describe GramsController, type: :controller do
 		it "should sucessfully create a new gram in our database" do
 			post :create, gram: {message: 'Hello!'}
 			expect(response).to redirect_to root_path
+
 			gram = Gram.last
 			expect(gram.message).to eq("Hello!")
 		end
+
+		it "should properly deal with validation errors" do
+			post :create, gram: { message: '' }
+			expect(response).to have_http_status(:unprocessable_entity)
+			
+			expect(Gram.count).to eq 0
+		end
+
 	end
 
 end
